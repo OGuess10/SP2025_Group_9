94ea6d4b540297ad0216acff73dd5aed1ff03566
OGuess10 <66238560+OGuess10@users.noreply.github.com>
2025-02-09 19:07:54 -0600

    Fixed folder issues. Tested initial backend/frontend communication

--- frontend/app/screens/HomeScreen.js
+++ frontend/app/screens/HomeScreen.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
+  const [test, setTest] = useState("");
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/')
+    .then(response => response.json())
+    .then(data => setTest(data.message));
+  }, []);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to the Home Page</Text>
+      <Text>{test}</Text>
     </View>
   );
 }
